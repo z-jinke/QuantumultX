72fbf33f06056b0e58942361a4b2f87fed5375c0
z-jinke <174172288+z-jinke@users.noreply.github.com>
2024-07-20 00:04:08 +0800

    Update Coolapk.js

--- scripts/Coolapk.js
+++ scripts/Coolapk.js
@@ -6,16 +6,17 @@ let obj = JSON.parse(body);
 
 // 匹配初始化接口
 if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
-    const entityIdSet = new Set([1742, 1966, 1681, 1633, 1710, 1754, 1229, 413, 417, 790, 813, 2191, 845, 2258, 1170, 1247, 2299, 2412, 2018]);
+    const entityIdSet = new Set([ 1742, 1966, 1681, 1633, 1710, 1754, 1229, 413, 417, 790, 813,2191， 845, 2258, 1170, 1247, 2299, 2412, 2018]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data.forEach(item => {
-            // 设置启动广告参数
             if (item.extraDataArr) {
                 item.extraDataArr["SplashAd.timeout"] = "0";
                 item.extraDataArr["SplashAd.Expires"] = 9999999999;
             }
             if (Array.isArray(item.entities)) {
-                item.entities = item.entities.filter(entity => !entityIdSet.has(entity.entityId));
+                item.entities = item.entities.filter(
+                    entity => !entityIdSet.has(entity.entityId)
+                );
             }
         });
     }
@@ -26,7 +27,9 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     const filterIds = new Set([32557, 13635, 29349, 16977]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data = obj.data.filter(item => !filterIds.has(item.entityId));
-        obj.data.forEach(item => delete item.extraDataArr);
+        obj.data.forEach(item => {
+            delete item.extraDataArr;
+        });
     }
     $done({ body: JSON.stringify(obj) });
 
@@ -42,7 +45,8 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
     const filterIds = new Set([12315, 8364, 14379, 24309, 35846, 35730, 12889]);
     if (obj.data && Array.isArray(obj.data)) {
-        obj.data.forEach(item => delete item.extraDataArr);
+        obj.data.forEach(item => {
+            delete item.extraDataArr; });
         obj.data = obj.data.filter(item => !filterIds.has(item.entityId));
     }
     $done({ body: JSON.stringify(obj) });
