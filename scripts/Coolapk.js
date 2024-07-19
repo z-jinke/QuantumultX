9d329360b2a3033de5276b0ad847a06f06fbcf17
z-jinke <174172288+z-jinke@users.noreply.github.com>
2024-07-20 00:30:00 +0800

    Update Coolapk.js

--- scripts/Coolapk.js
+++ scripts/Coolapk.js
@@ -1,11 +1,11 @@
 // 2024.7.18
 
-const url = $request.url;
+const url  = $request.url;
 const body = $response.body;
-let obj = JSON.parse(body);
 
 // 匹配初始化接口
 if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
+    let obj = JSON.parse(body);
     const entityIdSet = new Set([1742, 1966, 1681, 1633, 1710, 1754, 1229, 413, 417, 790, 813, 2191, 845, 2258, 1170, 1247, 2299, 2412, 2018]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data.forEach(item => {
@@ -24,6 +24,7 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
 
 // 匹配首页或搜索接口
 } else if (url.includes('/v6/main/indexV8') || url.includes('/v6/search?')) {
+    let obj = JSON.parse(body);
     const filterIds = new Set([32557, 13635, 29349, 16977]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data = obj.data.filter(item => !filterIds.has(item.entityId));
@@ -35,6 +36,7 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
 
 // 匹配账户配置接口
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
+    let obj = JSON.parse(body);
     const filterIds = new Set([1002, 1005, 14809, 1004]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data = obj.data.filter(item => !filterIds.has(item.entityId));
@@ -43,10 +45,12 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
 
 // 匹配信息流数据接口
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
+    let obj = JSON.parse(body);
     const filterIds = new Set([12315, 8364, 14379, 24309, 35846, 35730, 12889]);
     if (obj.data && Array.isArray(obj.data)) {
         obj.data.forEach(item => {
-            delete item.extraDataArr; });
+            delete item.extraDataArr;
+        });
         obj.data = obj.data.filter(item => !filterIds.has(item.entityId));
     }
     $done({ body: JSON.stringify(obj) });
