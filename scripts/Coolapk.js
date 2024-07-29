3da4c673e084e806296148c1418913413b99791e
z-jinke <174172288+z-jinke@users.noreply.github.com>
2024-07-29 20:38:53 +0800

    Update Coolapk.js

--- scripts/Coolapk.js
+++ scripts/Coolapk.js
@@ -3,7 +3,7 @@
 const url  = $request.url;
 const body = $response.body;
 
-// 匹配初始化接口
+// 初始化配置
 if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     let obj = JSON.parse(body);
     const entityIdSet = new Set([1742, 1966, 1681, 1633, 1710, 1754, 1229, 413, 417, 790, 813, 2191, 845, 2258, 1170, 1247, 2299, 2412, 2018]);
@@ -22,7 +22,7 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     }
     $done({ body: JSON.stringify(obj) });
 
-// 匹配首页或搜索接口
+// 首页和搜索
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/(main\/indexV8|search)/.test(url)) {
     let obj = JSON.parse(body);
     const filterIds = new Set([32557, 13635, 29349, 16977]);
@@ -34,7 +34,7 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     }
     $done({ body: JSON.stringify(obj) });
 
-// 匹配账户配置接口
+// 我的页面
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
     let obj = JSON.parse(body);
     const filterIds = new Set([1002, 1005, 14809, 1004]);
@@ -43,7 +43,7 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     }
     $done({ body: JSON.stringify(obj) });
 
-// 匹配信息流数据接口
+// 信息流广告
 } else if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
     let obj = JSON.parse(body);
     const filterIds = new Set([12315, 8364, 14379, 24309, 35846, 35730, 12889]);
