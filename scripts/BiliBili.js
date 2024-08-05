8ca4963df0205e4cb9423aef1e89971217dd5766
z-jinke <174172288+z-jinke@users.noreply.github.com>
2024-08-05 21:20:42 +0800

    Update BiliBili.js

--- scripts/BiliBili.js
+++ scripts/BiliBili.js
@@ -50,7 +50,7 @@ if (/^https?:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test(url)) {
     $done({ body: JSON.stringify(obj) });
 
 // 番剧与影视页面   
-} else if (/^https?:\/\/api\.bilibili\.com\/pgc\/page\/(cinema|bangumi)/。test(url)) {
+} else if (/^https?:\/\/api\.bilibili\.com\/pgc\/page\/(cinema|bangumi)/.test(url)) {
     let obj = JSON.parse(body);
     const excludeModuleIds = new Set([1441, 248, 1455, 1633, 1639]);
     if (obj.result && obj.result.modules) {
